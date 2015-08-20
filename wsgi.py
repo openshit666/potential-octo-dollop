ab10ea085d6a696a6c1ff4174f3c309acab1a6b5
foro_2009 <foro_2009@live.com>
2015-08-20 10:21:37 +0200

    fullpower

--- wsgi.py
+++ wsgi.py
@@ -16,7 +16,7 @@ def application(environ, start_response):
     if environ['PATH_INFO'] == '/' and ItsMe is True:
         response_body = ['<a href="/xml/{}" download>{}</a><br>'.format(f,f) for f in files]
         response_body.append('</body></html>')
-        response_body = '<!DOCTYPE html><html><head><meta content="text/html; charset=UTF-8"/></head><body>'.join(response_body)
+        response_body = '<!DOCTYPE html><html><head><meta content="charset=UTF-8"/></head><body>'.join(response_body)
         ctype = 'text/html'
     elif environ['PATH_INFO'].startswith('/xml/') and '..' not in environ['PATH_INFO'] and environ['PATH_INFO'].split('/')[-1] in files and ItsMe is True:
         r = open(os.environ['OPENSHIFT_REPO_DIR'] + '/xml/' + environ['PATH_INFO'].split('/')[-1], 'r')
@@ -25,20 +25,21 @@ def application(environ, start_response):
         ctypes = {'json': 'application/json', 'xml': 'application/xml'}
         ctype = ctypes[environ['PATH_INFO'].split('.')[-1]]
     else:
-        response_body = '''<!DOCTYPE html><html><head><meta content="text/html; charset=UTF-8"/></head><body>¿login?</body></html>'''
-        ctype = 'text/html'
+        response_body = '''<!DOCTYPE html><html><head><meta content="charset=UTF-8"/></head><body>¿login?</body></html>'''
+        ctype = 'text/html; charset=UTF-8'
 
-    # always It's OK, okeeeya
+    # always It's OK, okeeeya!?
     status = '200 OK'
 
     # create a cookie object
-    cookie = SimpleCookie()
-    cookie['session'] = 'ElCaloret'
-    cookie['session']['max-age'] = '600'
-    cookieheaders = ('Set-Cookie', cookie['session'].OutputString())
+#    cookie = SimpleCookie()
+#    cookie['session'] = 'ItsMe'
+#    cookie['session']['path'] = "/"
+#    cookie['session']['max-age'] = '864000'
+#    cookieheaders = ('Set-Cookie', cookie['session'].OutputString())
+#    response_headers = [cookieheaders, ('Content-Type', ctype), ('Content-Length', str(len(response_body)))]
 
-    response_headers = [cookieheaders, ('Content-Type', ctype), ('Content-Length', str(len(response_body)))]
-#    response_headers = [('Content-Type', ctype), ('Content-Length', str(len(response_body)))]
+    response_headers = [('Content-Type', ctype), ('Content-Length', str(len(response_body)))]
     start_response(status, response_headers)
     return [response_body]
 
