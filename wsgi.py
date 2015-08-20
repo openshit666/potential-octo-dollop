189a063d696f56eced4a5e0a5c38d91cc2253e46
foro_2009 <foro_2009@live.com>
2015-08-20 09:19:06 +0200

    Goes static files!!

--- wsgi.py
+++ wsgi.py
@@ -4,32 +4,40 @@ import os
 
 def application(environ, start_response):
 
-    ctype = 'text/plain'
-    if environ['PATH_INFO'] == '/health':
-        response_body = "1"
-    elif environ['PATH_INFO'] == '/env':
-        response_body = ['%s: %s' % (key, value)
-                    for key, value in sorted(environ.items())]
-        response_body = '\n'.join(response_body)
-    elif environ['PATH_INFO'] == '/asd':
-        r = open(os.environ['OPENSHIFT_REPO_DIR']+'/xml/dvdr-_-movies.xml', 'r')
+    ItsMe = False
+    response_body = None
+    files = os.path(os.environ['OPENSHIFT_REPO_DIR']+'/xml')
+
+    if 'HTTP_COOKIE' in environ:
+        rcookie = SimpleCookie(environ['HTTP_COOKIE'])
+        if 'session' in rcookie and rcookie['session'].value == 'ItsMe':
+            ItsMe = True
+
+    if environ['PATH_INFO'] == '/' and ItsMe is True:
+        response_body = ['<a href="/xml/{}" download>{}</a><br>'.format(f,f) for f in files]
+        response_body = ''.join(response_body)
+        ctype = 'text/html'
+    elif environ['PATH_INFO'].startswith('/xml/') and '..' not in environ['PATH_INFO'] and environ['PATH_INFO'].split('/')[-1] in files and ItsMe is True:
+        r = open(os.environ['OPENSHIFT_REPO_DIR'] + '/xml/' + environ['PATH_INFO'].split('/')[-1], 'r')
         response_body = r.read()
         r.close()
-        ctype = 'application/json'
+        ctypes = {'json': 'application/json', 'xml': 'application/xml'}
+        ctype = ctypes[environ['PATH_INFO'].split('.')[-1]]
     else:
+        response_body = '''¿login?'''
         ctype = 'text/html'
-        response_body = '''opensheit!'''
 
+    # always It's OK, okeeeya
     status = '200 OK'
 
     # create a cookie object
-    cookie = SimpleCookie()
-    cookie['likes'] = "cheese"
-    cookieheaders = ('Set-Cookie', cookie['likes'].OutputString())
+    #cookie = SimpleCookie()
+    #cookie['session'] = 'ElCaloret'
+    #cookie['session']['max-age'] = '600'
+    #cookieheaders = ('Set-Cookie', cookie['session'].OutputString())
 
-
-    response_headers = [cookieheaders, ('Content-Type', ctype), ('Content-Length', str(len(response_body)))]
-    #
+    #response_headers = [cookieheaders, ('Content-Type', ctype), ('Content-Length', str(len(response_body)))]
+    response_headers = [('Content-Type', ctype), ('Content-Length', str(len(response_body)))]
     start_response(status, response_headers)
     return [response_body.encode('utf-8') ]
 
