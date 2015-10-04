0f9c6d17fde51f11c3a2b372af791a6286970943
foro_2009 <foro_2009@live.com>
2015-10-04 20:54:26 +0200

    fix xml

--- scripts/lostoros.py
+++ scripts/lostoros.py
@@ -13,18 +13,22 @@ logging.basicConfig(level=logging.DEBUG,
                     filename=pathlog+'scripts.log')
 
 soup = BeautifulSoup(open(pathrepo+'xml/lostoros.xml'))
-links = soup.find_all('link')[1:]
-regexplast = re.search('\d{4}/\d{1,2}/\d{1,2}', links[0].get_text()).group()
+guids = soup.find_all('guid')
+regexplast = re.search('\d{4}/\d{1,2}/\d{1,2}', guids[0].get_text()).group()
 hoy = strftime('%Y/%-m/%-d', localtime())
 
 if regexplast != hoy:
-    for l in links:
-        text = re.sub('\d{4}/\d{1,2}/\d{1,2}', hoy, l.get_text())
+    for g in guids:
+        text = re.sub('\d{4}/\d{1,2}/\d{1,2}', hoy, g.get_text())
         text = re.sub('s_\d{8}', 's_' + strftime('%Y%m%d', localtime()), text)
-        l.string = text
+        g.string = text
     for t in soup.find_all('title')[1:]:
         text = re.sub('\d{2}/\d{2}/\d{4}', strftime('%d/%m/%Y', localtime()), t.get_text())
         t.string = text
+    for c in soup.find_all('enclosure'):
+        text = re.sub('\d{4}/\d{1,2}/\d{1,2}', hoy, c.get('url'))
+        text = re.sub('s_\d{8}', 's_' + strftime('%Y%m%d', localtime()), text)
+        c['url'] = text
     soup.lastbuilddate.string = strftime('%a, %d %h %Y %H:%m:%S GMT', localtime())
     with open('py/lostoros.xml', "wb") as f:
         f.write(soup.renderContents())
