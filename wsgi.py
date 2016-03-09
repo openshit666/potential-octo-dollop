82a78bc69f1465c40c582a07e9b665a0c94b045d
foro_2009 <foro_2009@live.com>
2016-03-09 02:50:02 +0100

    asd

--- wsgi.py
+++ wsgi.py
@@ -9,44 +9,46 @@ import os
 def application(environ, start_response):
     ItsMe = False
     response_body = None
+    path = os.path.normpath(environ['PATH_INFO'])
     files = os.listdir(os.environ['OPENSHIFT_DATA_DIR'] + 'xml')
     shows = getpls(None).allpro
+    print(environ['wsgi.input'])
 
     if 'HTTP_COOKIE' in environ:
         rcookie = SimpleCookie(environ['HTTP_COOKIE'])
         if 'session' in rcookie and rcookie['session'].value == 'ItsMe':
             ItsMe = True
-    if environ['PATH_INFO'] == '/' and ItsMe is True:
+    if path == '/' and ItsMe is True:
         response_body = ['<tr><td style="text-align:left;"><a href="/xml/{}" download>{}</a></td><td style="text-align:right;">{} kB</td><td style="text-align:right;">{}</td></tr>'.format(f, f, round(os.stat(os.environ['OPENSHIFT_DATA_DIR'] + 'xml/' + f).st_size / 1024, 1), strftime('%-d/%m at %H:%M', localtime(os.stat(os.environ['OPENSHIFT_DATA_DIR'] + 'xml/' + f).st_mtime))) for f in files]
         response_body.append('''<tr><td style="text-align:center;padding-top:25px;"><button onclick="go('/daily');">Daily</button></td><td></td><td style="text-align:center;padding-top:25px;"><button onclick="go('/hourly');">Hourly</button></td></tr></table></center><script type="text/javascript">function changetext(text){over=document.querySelector("#over");document.querySelector("#result").textContent=text;over.style.display="block";setTimeout(function(){over.style.display="none";location.reload();},2e3);}function go(cual){var xmlhttp=new XMLHttpRequest();xmlhttp.open("GET",cual);xmlhttp.onreadystatechange=function(){if(xmlhttp.readyState==4&&xmlhttp.status==200)   {changetext(xmlhttp.responseText);}else{changetext(xmlhttp.statusText+" "+xmlhttp.status);}};xmlhttp.send(null);}</script></body></html>''')
         response_body.insert(0, '<!DOCTYPE html><html><head><meta content="charset=UTF-8"/><title>pi-ton</title></head><style>td {padding: 3px;}</style><body><center><div id="over"style="display:none;position:fixed;top:0%;left:0%;width:100%;height:100%;background-color:black;-moz-opacity:0.8;opacity:.80;filter:alpha(opacity=80);"><p id="result"style="color:red;margin-top:20%;font-weight:bolder;font-size:25px;"></p></div><table style="margin-top:8%;"><th>Archivo</th><th>Tamaño</th><th style="width:150px;text-align: right;">Fecha modif.</th>')
         response_body = ''.join(response_body)
         ctype = 'text/html; charset=UTF-8'
-    elif environ['PATH_INFO'] == '/' and ItsMe is False:
+    elif path == '/' and ItsMe is False:
         response_body = '''<!DOCTYPE html><html><head><meta content="charset=UTF-8"/><title>pi-ton</title></head><body><center><input type="text"size="10"placeholder="And you are...?"style="margin-top:20%;text-align:center"autofocus required></center><script type="text/javascript">document.querySelector("input").addEventListener("keypress",function(e){if(e.key=="Enter"||e.keyCode=="13"){document.cookie="session="+document.querySelector("input").value+"; max-age=864000; path=/";location.reload();}});</script></body></html>'''
         ctype = 'text/html; charset=UTF-8'
-    elif environ['PATH_INFO'].startswith('/xml/') and '..' not in environ['PATH_INFO'] and environ['PATH_INFO'].split('/')[-1] in files and ItsMe is True:
-        r = open(os.environ['OPENSHIFT_DATA_DIR'] + 'xml/' + environ['PATH_INFO'].split('/')[-1], 'r')
+    elif path.startswith('/xml/') and path.split('/')[-1] in files and ItsMe is True:
+        r = open(os.environ['OPENSHIFT_DATA_DIR'] + 'xml/' + path.split('/')[-1], 'r')
         response_body = r.read()
         r.close()
         ctypes = {'json': 'application/json; charset=UTF-8', 'xml': 'application/xml; charset=UTF-8'}
-        ctype = ctypes[environ['PATH_INFO'].split('.')[-1]]
-#    elif environ['PATH_INFO'] == '/xml/lostoros.xml':
+        ctype = ctypes[path.split('.')[-1]]
+#    elif path == '/xml/lostoros.xml':
 #        r = open(os.environ['OPENSHIFT_DATA_DIR'] + 'xml/lostoros.xml', 'r')
 #        response_body = r.read()
 #        r.close()
 #        ctype = 'application/xml; charset=UTF-8'
-    elif environ['PATH_INFO'].endswith('.pls') and environ['PATH_INFO'].startswith('/pls/') and '..' not in environ['PATH_INFO'] and environ['PATH_INFO'].split('/')[-1].replace('.pls', '') in shows:
-        response_body = getpls(environ['PATH_INFO'].split('/')[-1].replace('.pls', '')).joinedpls
+    elif path.endswith('.pls') and path.startswith('/pls/') and path.split('/')[-1].replace('.pls', '') in shows:
+        response_body = getpls(path.split('/')[-1].replace('.pls', '')).joinedpls
         ctype = 'audio/x-scpls'
-    elif environ['PATH_INFO'] == '/daily' or environ['PATH_INFO'] == '/hourly' and ItsMe is True:
-        sp = cc(['sh', './app-root/repo/.openshift/cron/{}/runner'.format(environ['PATH_INFO']), 'echo'])
+    elif path == '/daily' or path == '/hourly' and ItsMe is True:
+        sp = cc(['sh', './app-root/repo/.openshift/cron/{}/runner'.format(path), 'echo'])
         if sp == 0:
             response_body = 'ok'
         else:
             response_body = 'fail'
         ctype = 'text/html; charset=UTF-8'
-#    elif environ['PATH_INFO'] == '/env.pls':
+#    elif path == '/env.pls':
 #        ctype = 'text/plain'
 #        response_body = ['%s: %s' % (key, value) for key, value in sorted(environ.items())]
 #        response_body.append('SCRIPT_NAME: {}'.format(environ['SCRIPT_NAME']))
