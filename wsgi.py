6c57ac780bd869bb6abcfd2c62bef4d3879dcc0b
foro_2009 <foro_2009@live.com>
2016-03-11 08:04:26 +0100

    basic http auth

--- wsgi.py
+++ wsgi.py
@@ -8,18 +8,20 @@ import os
 
 def application(environ, start_response):
     ItsMe = False
+    auth = False
     response_body = None
     path = os.path.normpath(environ['PATH_INFO'])
     files = os.listdir(os.environ['OPENSHIFT_DATA_DIR'] + 'xml')
     shows = getpls(None).allpro
-
-    asd = ['%s: %s' % (key, value) for key, value in sorted(environ.items())]
-    print('\n'.join(asd))
     
     if 'HTTP_COOKIE' in environ:
         rcookie = SimpleCookie(environ['HTTP_COOKIE'])
         if 'session' in rcookie and rcookie['session'].value == 'ItsMe':
             ItsMe = True
+    if 'HTTP_AUTHORIZATION' in environ:
+        if environ['HTTP_AUTHORIZATION'].split(' ')[-1] == 'cGktdG9uOmVsY2Fsb3JldA==':
+            auth = True
+#            'pi-ton:elcaloret', 'cGktdG9uOmVsY2Fsb3JldA=='
     if path == '/' and ItsMe is True:
         response_body = ['<tr><td style="text-align:left;"><a href="/xml/{}" download>{}</a></td><td style="text-align:right;">{} kB</td><td style="text-align:right;">{}</td></tr>'.format(f, f, round(os.stat(os.environ['OPENSHIFT_DATA_DIR'] + 'xml/' + f).st_size / 1024, 1), strftime('%-d/%m at %H:%M', localtime(os.stat(os.environ['OPENSHIFT_DATA_DIR'] + 'xml/' + f).st_mtime))) for f in files]
         response_body.append('''<tr><td style="text-align:center;padding-top:25px;"><button onclick="go('/daily');">Daily</button></td><td></td><td style="text-align:center;padding-top:25px;"><button onclick="go('/hourly');">Hourly</button></td></tr></table></center><script type="text/javascript">function changetext(text){over=document.querySelector("#over");document.querySelector("#result").textContent=text;over.style.display="block";setTimeout(function(){over.style.display="none";location.reload();},2e3);}function go(cual){var xmlhttp=new XMLHttpRequest();xmlhttp.open("GET",cual);xmlhttp.onreadystatechange=function(){if(xmlhttp.readyState==4&&xmlhttp.status==200)   {changetext(xmlhttp.responseText);}else{changetext(xmlhttp.statusText+" "+xmlhttp.status);}};xmlhttp.send(null);}</script></body></html>''')
@@ -38,8 +40,7 @@ def application(environ, start_response):
                 response_headers = [cookieheaders, ('Location', '/')]
                 start_response('302 Found', response_headers)
                 return ['1']     
-            else:
-                raise Exception
+            raise Exception
         except:
             response_body = '''<!DOCTYPE html><html><head><meta content="charset=UTF-8"/><title>pi-ton</title></head><body><center><form action=""method="post"><input name="session"type="text"size="10"placeholder="And you are...?"style="margin-top:20%;text-align:center"autofocus required><input type="submit"value="Submit"style="display:none"></form></center></body></html>'''
             ctype = 'text/html; charset=UTF-8'
@@ -55,53 +56,41 @@ def application(environ, start_response):
 #        r.close()
 #        ctype = 'application/xml; charset=UTF-8'
     elif path.startswith('/pls/') and path.endswith('.pls') and path.split('/')[-1].replace('.pls', '') in shows:
-        response_body = getpls(path.split('/')[-1].replace('.pls', '')).joinedpls
+        if ItsMe is True or Auth is True:
+            response_body = getpls(path.split('/')[-1].replace('.pls', '')).joinedpls
         ctype = 'audio/x-scpls'
     elif path == '/daily' or path == '/hourly' and ItsMe is True:
         sp = cc(['sh', './app-root/repo/.openshift/cron/{}/runner'.format(path), 'echo'])
         if sp == 0:
             response_body = 'ok'
-        else:
-            response_body = 'fail'
+        response_body = 'fail'
         ctype = 'text/html; charset=UTF-8'
-    elif path == '/env':
+#    elif path == '/env':
 #        ctype = 'text/plain'
 #        response_body = ['%s: %s' % (key, value) for key, value in sorted(environ.items())]
 #        response_body.append('SCRIPT_NAME: {}'.format(environ['SCRIPT_NAME']))
 #        response_body = '\n'.join(response_body)
-        body = 'Please authenticate'
-        headers = [
-            ('content-type', 'text/plain'),
-            ('content-length', str(len(body))),
-            ('WWW-Authenticate', 'Basic realm="asd"')]
-        start_response('401 Unauthorized', headers)
-        return [body]
     else:
         if ItsMe is True:
             start_response('302 Found', [('Location', '/')])
-        else:
-            start_response('302 Found', [('Location', '/login')])
+        start_response('302 Found', [('Location', '/login')])
         return ['1']
-
 
     # always It's OK, okeeeya!?
     status = '200 OK'
 
-    # create a cookie object
-#    cookie = SimpleCookie()
-#    cookie['session'] = 'ItsMe'
-#    cookie['session']['path'] = "/"
-#    cookie['session']['max-age'] = '864000'
-#    cookieheaders = ('Set-Cookie', cookie['session'].OutputString())
-#    response_headers = [cookieheaders, ('Content-Type', ctype), ('Content-Length', str(len(response_body)))]
     if ctype == 'audio/x-scpls':
+        if auth is False:
+            response_body = '''<!DOCTYPE html><html><head><meta charset="UTF-8"/><title>Error</title></head><body><h1>401 Unauthorized.</h1><h2><a href="/login">login?</a></h2></body></html>'''
+            response_headers = [('content-type', 'text/html; charset=UTF-8'), ('content-length', str(len(response_body.encode('utf8')))), ('WWW-Authenticate', 'Basic realm="pls@pi-ton"')]
+            start_response('401 Unauthorized', response_headers)
+            return [response_body.encode('utf8')]
         response_headers = [('Content-Type', ctype)]
         start_response(status, response_headers)
         return [response_body.encode()]
-    else:
-        response_headers = [('Content-Type', ctype), ('Content-Length', str(len(response_body.encode('utf8'))))]
-        start_response(status, response_headers)
-        return [response_body.encode('utf8')]
+    response_headers = [('Content-Type', ctype), ('Content-Length', str(len(response_body.encode('utf8'))))]
+    start_response(status, response_headers)
+    return [response_body.encode('utf8')]
 #    return [response_body]
 
 #
