f022461fd1b6814301c71b47fdd49c29cd2a931b
foro_2009 <foro_2009@live.com>
2016-11-12 21:50:58 +0100

    asdfix feeds

--- scripts/pls.py
+++ scripts/pls.py
@@ -1,11 +1,12 @@
 from bs4 import BeautifulSoup
 from random import choice
 import urllib.request
+import urllib.error
 
 
 class getpls:
     def __init__(self, show):
-        self.programas = {'wdm': {'radio': 'Los40', 'feed': 'http://urotrosfiles.media.streamtheworld.com/otrosfiles/podcasts/483.xml'}, 'nano': {'radio': 'LaMáxima', 'feed': 'http://www.radioasturias.com/rss_audios.asp?emisora=Maxima+FM+Asturias&programa=Bien+Bailao'}, 'carl': {'radio': 'LaMáxima', 'feed': 'http://www.radioasturias.com/rss_audios.asp?emisora=Maxima+FM+Asturias&programa=Global+Carl+Cox'}, 'armin': {'radio': 'LaMáxima', 'feed': 'http://www.radioasturias.com/rss_audios.asp?emisora=Maxima+FM+Asturias&programa=F%C3%B3rmula+M%C3%A1xima'}, 'insessions': {'radio': 'LaMáxima', 'feed': 'http://www.radioasturias.com/rss_audios.asp?emisora=Maxima+FM+Asturias&programa=In+Sessions'}, 'insomnia': {'radio': 'EuropaFM', 'feed': 'http://www.europafm.com/rss/podcast/236303/podcast.xml'}, 'climax': {'radio': 'LaMáxima', 'feed': 'http://www.radioasturias.com/rss_audios.asp?emisora=Maxima+FM+Asturias&programa=Cl%C3%ADmax+Chill+Out'}, 'chillout': {'radio': 'EuropaFM', 'feed': 'http://www.europafm.com/rss/podcast/236321/podcast.xml'}, 'europa': {'radio': 'EuropaFM', 'feed': 'http://www.europafm.com/rss/podcast/236312/podcast.xml'}, 'brian': {'radio': 'EuropaFM', 'feed': 'http://www.europafm.com/rss/podcast/256645/podcast.xml'}, 'atodojazz': {'radio': 'RNE', 'feed': 'http://www.rtve.es/alacarta/interno/contenttable.shtml?pbq={}&orderCriteria=DESC&modl=TOC&locale=es&pageSize=15&ctx=1875'.format(choice(range(1, 45)))}, 'jazzporquesi': {'radio': 'RNE', 'feed': 'http://www.rtve.es/alacarta/interno/contenttable.shtml?pbq={}&orderCriteria=DESC&modl=TOC&locale=es&pageSize=15&ctx=1999'.format(choice(range(1, 58)))}}
+        self.programas = {'wdm': {'radio': 'Los40', 'feed': 'http://urotrosfiles.media.streamtheworld.com/otrosfiles/podcasts/483.xml'}, 'nano': {'radio': 'LaMáxima', 'feed': 'http://urotrosfiles.media.streamtheworld.com/otrosfiles/podcasts/767.xml'}, 'armin': {'radio': 'LaMáxima', 'feed': 'https://miroppb.com/rss'}, 'insomnia': {'radio': 'EuropaFM', 'feed': 'http://www.europafm.com/rss/podcast/236303/podcast.xml'}, 'climax': {'radio': 'LaMáxima', 'feed': 'http://urotrosfiles.media.streamtheworld.com/otrosfiles/podcasts/707.xml'}, 'chillout': {'radio': 'EuropaFM', 'feed': 'http://www.europafm.com/rss/podcast/236321/podcast.xml'}, 'europa': {'radio': 'EuropaFM', 'feed': 'http://www.europafm.com/rss/podcast/236312/podcast.xml'}, 'atodojazz': {'radio': 'RNE', 'feed': 'http://www.rtve.es/alacarta/interno/contenttable.shtml?pbq={}&orderCriteria=DESC&modl=TOC&locale=es&pageSize=15&ctx=1875'.format(choice(range(1, 45)))}, 'jazzporquesi': {'radio': 'RNE', 'feed': 'http://www.rtve.es/alacarta/interno/contenttable.shtml?pbq={}&orderCriteria=DESC&modl=TOC&locale=es&pageSize=15&ctx=1999'.format(choice(range(1, 58)))}, 'tatw': {'radio': 'LaMáxima', 'feed': 'https://cdn.trancearoundtheworld.co.uk/podcasts/tatw/feed.xml'}, 'gtr': {'radio': 'LaMáxima', 'feed': 'http://static.aboveandbeyond.nu/grouptherapy/podcast.xml'}, 'tks': {'radio': 'LaMáxima', 'feed': 'http://www.djvytl.com/karandashow/'}}
         self.allpro = list(self.programas.keys()) + ['random', 'randomdance', 'randomjazz']
         if show == 'random':
             self.show = choice(list(self.programas.keys()))
@@ -28,26 +29,33 @@ class getpls:
         radio = self.programas[self.show]['radio']
         req = urllib.request.Request(url)
         req.add_header('User-Agent', 'Mozilla/5.0')
-        f = urllib.request.urlopen(req)
+        z = 0
+        while z < 11:
+            z+=1
+            try:
+                f = urllib.request.urlopen(req)
+                break
+            except urllib.error.URLError as e:
+                if str(e.reason) != '[Errno 104] Connection reset by peer':
+                    return None
+
         soup = BeautifulSoup(f.read().decode(), "html.parser")
         audios = soup.find_all('guid')
+        Naudios = soup.find_all('enclosure')
 
-        if self.show == 'insessions':
-            ultis = list(range(-7, 0))
-            uno = choice(ultis)
-            ultis.remove(uno)
-            dos = choice(ultis)
-            aa = sorted([audios[uno].get_text(), audios[dos].get_text()])
-            audio = [aa[0], aa[1]]
-        elif self.show == 'armin':
-            ttaa = [(d.find_previous_siblings('title')[0].get_text().replace('Fórmula Máxima', 'A State of Trance'), d.find_next_siblings('guid')[0].get_text()) for d in soup.find_all('description') if 'domingo' in d.get_text() and '22:00' in d.get_text() or 'domingo' in d.get_text() and '23:00' in d.get_text()]
-            audio = [ttaa[-2][1], ttaa[-1][1]]
+        if self.show == 'armin' or self.show == 'gtr':
+            audio = [Naudios[0].get('url')]
         elif self.show == 'climax':
-            audio = [audios[-2].get_text().replace('05-00', '02-00').replace('04-00', '01-00'), audios[-1].get_text().replace('05-00', '02-00').replace('04-00', '01-00')]
-        elif self.show == 'wdm':
+            audio = [audios[2].get_text(), audios[1].get_text(), audios[0].get_text()]
+        elif self.show == 'wdm' or self.show == 'nano':
             audio = [audios[1].get_text(), audios[0].get_text()]
-        elif radio == 'LaMáxima':
-            audio = [audios[-2].get_text(), audios[-1].get_text()]
+        elif show == 'tatw':
+            i = choice(range(len(Naudios)))
+            audio = [Naudios[i - 1].get('url')]
+        elif show == 'tks':
+            Ntitles = soup.find_all('a')[5:103]
+            i = choice(range(len(Ntitles)))
+            audio = ['{}{}'.format(url, Ntitles[i].get('href'))]
         else:
             audio = [audios[0].get_text()]
         self.joinedpls = '[playlist]\n{}\nTitle{}={}\nNumberOfEntries={}\nVersion=2'.format('\n'.join(['File{}={}'.format(i + 1, audio[i]) for i in range(len(audio))]), len(audio), self.show, len(audio))
