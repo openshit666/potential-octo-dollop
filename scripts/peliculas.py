35b3f93b3acbd1e34779a434f82fb9671771d748
foro_2009 <foro_2009@live.com>
2017-03-16 04:31:57 +0100

    todotorrents down

--- scripts/peliculas.py
+++ scripts/peliculas.py
@@ -5,33 +5,35 @@ import PyRSS2Gen
 import datetime
 import logging
 import os
-import re
+
+
+def get_torrent(url):
+    req = urllib.request.Request('http://www.moviesdvdr.com/{}'.format(url))
+    req.add_header('User-Agent', 'Mozilla/5.0')
+    r = urllib.request.urlopen(req)
+    soup = BeautifulSoup(r.read().decode('iso-8859-1'), "html.parser")
+    return 'http://www.moviesdvdr.com/{}'.format(soup.find(string='Descargar TORRENT').parent.get('href'))
+
 
 pathlog = os.environ['OPENSHIFT_LOG_DIR']
 pathrepo = os.environ['OPENSHIFT_REPO_DIR']
-
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s :: DVDR :: %(levelname)s :: %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S',
                     filename=pathlog + 'report.log')
 
-soup = BeautifulSoup(open(pathrepo + 'xml/dvdr-_-movies.xml'))
+soup = BeautifulSoup(open(pathrepo + 'xml/dvdr-_-movies.xml'), "html.parser")
 L_titulos = soup.find_all('title')
 L_titulos = [L_titulos[i].string for i in range(1, len(L_titulos))]
 L_enlaces = soup.find_all('link')
 L_enlaces = [L_enlaces[i].string for i in range(1, len(L_enlaces))]
-
-url = 'http://todotorrents.com'
-data = urllib.parse.urlencode({'Submit2': 'Buscar', 'buscar': 'MoviesDVDR', 'cate': '3', 'estado': '1'})
-data = data.encode('iso-8859-1')
+url = 'http://www.moviesdvdr.com/'
 req = urllib.request.Request(url)
 req.add_header('User-Agent', 'Mozilla/5.0')
-req.add_header('Content-Type', 'application/x-www-form-urlencoded;charset=iso-8859-1')
-r = urllib.request.urlopen(req, data)
-soup = BeautifulSoup(r.read().decode('iso-8859-1'))
-a_titles = soup.find_all(href=re.compile('/descargar'))
-W_enlaces = ['http://todotorrents.com/download.php?id=' + re.findall('/descargar/(.*)/', v.get('href'))[0] for v in a_titles if 'DVD9' not in v.get_text()]
-W_titulos = [v.get('title').split('DVDR')[0].strip() for v in a_titles if 'DVD9' not in v.get_text()]
+r = urllib.request.urlopen(req)
+soup = BeautifulSoup(r.read().decode('iso-8859-1'), "html.parser")
+W_titulos = [t.get('alt') for t in soup.find_all('img', alt=True, width=True)]
+W_enlaces = [get_torrent(t.parent.get('href')) for t in soup.find_all('img', alt=True, width=True)]
 
 if W_titulos[0] != L_titulos[0]:
     e = W_titulos.index(L_titulos[0])
